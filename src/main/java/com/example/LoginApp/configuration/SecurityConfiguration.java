fcd6a6ac70774c7485cdc6404bb1317a2ad47037
ester <estera.skana01@gmail.com>
2023-07-08 14:53:21 +0200

    cors added

--- src/main/java/com/example/LoginApp/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/LoginApp/configuration/SecurityConfiguration.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -42,4 +47,18 @@ public class SecurityConfiguration {
         return httpSecurity.build();
     }
 
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://skanahr.com")); // Set the allowed origin
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTION")); // Set the allowed methods
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization")); // Set the allowed headers
+        configuration.setAllowCredentials(true); // Allow credentials (e.g., cookies)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Apply the configuration to all paths
+        return source;
+    }
 }
+
