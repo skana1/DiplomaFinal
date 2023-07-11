9744cb07aa9cff8f4fcf3e340c4fd8850b688bf2
ester <estera.skana01@gmail.com>
2023-07-11 13:11:56 +0200

    users/me

--- src/main/java/com/example/LoginApp/controller/UserController.java
+++ src/main/java/com/example/LoginApp/controller/UserController.java
@@ -1,25 +1,28 @@
-//package com.example.LoginApp.controller;
-//
-//import com.example.LoginApp.Service.UserService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/v1/users")
-//@RequiredArgsConstructor
-//public class UserController {
-//    private final UserService userService;
-//
-//    @GetMapping("/me")
-//    public String getUser() {
-//        // Get the authentication details from the token.
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//
-//        // Here we return the user information based on it's role and logic of the service.
-//        return userService.getUserDetails(authentication.getName());
-//    }
-//}
+package com.example.LoginApp.controller;
+
+import com.example.LoginApp.Service.UserService;
+import com.example.LoginApp.models.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getUser() {
+        // Get the authentication details from the token.
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // Here we return the user information based on it's role and logic of the service.
+        User loggedUser = userService.getUserDetails(authentication.getName());
+        return new ResponseEntity<User>(loggedUser, HttpStatus.OK);
+    }
+}
--- src/main/java/com/example/LoginApp/service/UserService.java
+++ src/main/java/com/example/LoginApp/service/UserService.java
@@ -1,15 +1,20 @@
-//package com.example.LoginApp.Service;
-//import com.example.LoginApp.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class UserService {
-//
-//    private final UserRepository userRepository;
-//
-//    public String getUserDetails(String name) {
-//        return name;
-//    }
-//}
+package com.example.LoginApp.Service;
+import com.example.LoginApp.exception.CompanyNotFoundException;
+import com.example.LoginApp.exception.UserNotFoundException;
+import com.example.LoginApp.models.User;
+import com.example.LoginApp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User getUserDetails(String email) {
+        return userRepository.findAllByEmail(email).orElseThrow(() ->
+                new UserNotFoundException("User with email: " + email + " was not found"));
+
+    }
+}
